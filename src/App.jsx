92ad25f2d8c8a63c5b80ab12a2a2ef83369145f9
Moideen Mashad <moideenmashad@gmail.com>
2025-02-18 23:50:42 +0530

    Updated Navbar Coponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 import RoutesApp from "@/routes/RoutesApp";
 import "./index.css";
-import '@/assets/styles/font.css'
+import '@/assets/styles/font.scss';
 function App() {
   return (
     <div className="App">
--- src/components/layouts/Navbar/PreNavabar.jsx
+++ src/components/layouts/Navbar/PreNavabar.jsx
@@ -1,12 +1,25 @@
 // import React from "react";
 import { SlGlobe } from "react-icons/sl";
-
+import { GoSearch, GoChevronRight } from "react-icons/go";
+import "./Navbar.css";
 const PreNavabar = () => {
-  const Today = new Date().toDateString();
+  const today = new Date().toLocaleDateString("en-US", {
+    weekday: "long",
+    month: "long",
+    day: "numeric",
+    year: "numeric",
+  });
   return (
-    <div className="flex items-center">
-      <SlGlobe />
-      <p className="date">{Today}</p>
+    <div className="pre-navbar-container max-w-screen-xl mx-auto px-4 flex justify-between items-center border-primary border-y-2 my-2">
+      <div className="flex items-center pb-2 pt-2 space-x-1">
+        <SlGlobe />
+        <p className="text-primary">{today}</p>
+      </div>
+      <div className="flex items-center pb-2 pt-2 space-x-2">
+        <p className="text-primary">The Menu</p>
+        <GoChevronRight />
+        <GoSearch />
+      </div>
     </div>
   );
 };
