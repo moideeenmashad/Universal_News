f080316e028f30d018a1bd583bc74b4a3a9bd311
Moideen Mashad <moideenmashad@gmail.com>
2025-02-21 00:09:13 +0530

    Updated

--- src/pages/home/Featured.jsx
+++ src/pages/home/Featured.jsx
@@ -10,19 +10,19 @@ const Featured = () => {
     },
     {
       id: 2,
-      img: "https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638892472/EducationHub/photos/new-york-protesters.jpg",
+      img: "https://www.artificialintelligence-news.com/wp-content/uploads/2025/02/grok-3-ai-model-xai-reasoning-artificial-intelligence-benchmarks-elon-musk-development-ethics.jpg",
       title: "Technology",
       assert: "The latest trends in AI and innovation",
     },
     {
       id: 3,
-      img: "https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638892472/EducationHub/photos/new-york-protesters.jpg",
+      img: "https://www.aljazeera.com/wp-content/uploads/2024/08/2024-08-16T062607Z_498023022_RC213Z9IMEC1_RTRMADP_3_HEALTH-MPOX-PAKISTAN-1723793768.jpg?resize=770%2C513&quality=80",
       title: "Health",
       assert: "Analyzing the effects of global health policies",
     },
     {
       id: 4,
-      img: "https://images.nationalgeographic.org/image/upload/t_edhub_resource_key_image/v1638892472/EducationHub/photos/new-york-protesters.jpg",
+      img: "https://th-i.thgim.com/public/sport/xa9qka/article69243783.ece/alternates/FREE_1200/Ugo%20Blanchet.JPG",
       title: "Sports",
       assert: "Effects of cutting-edge wearables in professional sports",
     },
